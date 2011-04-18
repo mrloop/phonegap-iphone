cca9a97006c9d9889444cc5393da3f0295c766e4
Sam Elliott <sam@lenary.co.uk>
2011-04-18 11:07:14 +0100

    fix default image for iPad

--- PhoneGapLib/Classes/PhoneGapDelegate.m
+++ PhoneGapLib/Classes/PhoneGapDelegate.m
@@ -259,7 +259,12 @@ static NSString *gapVersion;
 	 * imageView - is the Default loading screen, it stay up until the app and UIWebView (WebKit) has completly loaded.
 	 * You can change this image by swapping out the Default.png file within the resource folder.
 	 */
-	UIImage* image = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Default" ofType:@"png"]];
+    UIImage* image;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        image = [UIImage imageNamed:@"Default-Portrait~ipad"];
+    } else {
+        image = [UIImage imageNamed:@"Default"];
+    }
 	imageView = [[UIImageView alloc] initWithImage:image];
 	[image release];
 	
